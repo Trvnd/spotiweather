18d7c205a1c6bd27e047db274be62cd89941db3c
River <+>
2022-05-11 18:01:02 -0400

    css and js update rk

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-// const searchUrl = ('https://metaweather.com/api/location/search/?query=(query)')
 
-// const locationUrl = ('https://metaweather.com/api/location/(woeid)/')
-
-// const dateUrl = ('https://metaweather.com/api/location/(woeid)/(date)/')
-
-
-
-/*document.addEventListener('DOMContentLoaded', () => {
-    fetch(`https://metaweather.com/api/location/search/?query=${city}`)
-        .then(res => res.json())
-        .then(data => data(json))
-})
-*/
-
-/*function fetchLocation(woeid) {
-    fetch('https//metaweather.com/api/location/(woeid)/${woeid}')
-        .then(data => {
-            console.log(data)
-            return data.json()
-        })
-        .then(result => {
-            const today = result.consolidated_weather[0]
-            console.log(`temperature in ${result.title} will stay between ${today.min_temp} and ${today.max_temp}`)
-        })
-}*/
-
-/*function fetchSearch() {
-    fetch(searchUrl)
-        .then(res => res.json())
-        .then(searchObj => search(searchObj))
-}
-*/
 document.addEventListener("DOMContentLoaded", function() {
+
+    const weatherState = document.createElement('h3')
+    const currentTemp = document.createElement('p')
+    const minTemp = document.createElement('p')
+    const maxTemp = document.createElement('p')
+    //const currentC = document.createElement('p')
+    //const minC = document.createElement('p')
+    //const maxC = document.createElement('p')
+    const weatherInfo = document.getElementById('weather-info')
+    const img = document.getElementById('img')
+
 const searchBar = document.getElementById('searchbar');
 searchBar.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -56,27 +35,63 @@ searchBar.addEventListener('submit', (e) => {
                 })
                     .then(res => res.json())
                     .then(data => weather(data))
+            }
+
+            function weather(data) {
+                weatherState.textContent = data.consolidated_weather[0].weather_state_name
+                currentTemp.textContent = Math.floor(data.consolidated_weather[0].the_temp)
+                minTemp.textContent = Math.floor(data.consolidated_weather[0].min_temp)
+                maxTemp.textContent = Math.floor(data.consolidated_weather[0].max_temp) 
+
+                weatherInfo.append(weatherState)
+
+                //weatherInfo.append(currentTemp)
+                //weatherInfo.append(minTemp)
+                //weatherInfo.append(maxTemp)
+
+
+                let currentC = currentTemp.textContent
+                let currentF = currentC * 9 / 5 + 32;
+                weatherInfo.append(currentF)
+
+                /*let createMC = document.createElement('p')
+                
+                let maxC = minTemp.textContent
+                let maxF = maxC * 9 / 5 + 32;
+                weatherInfo.append(maxF)
+
+                let minC = minTemp.textContent
+                let minF = minC * 9 / 5 + 32;
+                weatherInfo.append(minF)*/
+
+                weatherInfo.append(img)
+                if (weatherState.textContent === 'Clear') {
+                    img.src = 'http://openweathermap.org/img/wn/01d@2x.png'
+                } if (weatherState.textContent === 'Light Cloud') {
+                    img.src = 'http://openweathermap.org/img/wn/02d@2x.png'
+                } if (weatherState.textContent === 'Heavy Cloud') {
+                    img.src = 'http://openweathermap.org/img/wn/03d@2x.png'
+                } if (weatherState.textContent === 'Showers') {
+                    img.src = 'http://openweathermap.org/img/wn/09d@2x.png'
+                } if (weatherState.textContent === 'Light Rain') {
+                    img.src = 'http://openweathermap.org/img/wn/04d@2x.png'
+                } if (weatherState.textContent === 'Heavy Rain') {
+                    img.src = 'http://openweathermap.org/img/wn/09d@2x.png'
+                } if (weatherState.textContent === 'Thunderstorm') {
+                    img.src = 'http://openweathermap.org/img/wn/11d@2x.png'
+                } if (weatherState.textContent === 'Hail') {
+                    img.src = 'http://openweathermap.org/img/wn/13d@2x.png'
+                } if (weatherState.textContent === 'Sleet') {
+                    img.src = 'http://openweathermap.org/img/wn/13d@2x.png'
+                } if (weatherState.textContent === 'Snow') {
+                    img.src = 'http://openweathermap.org/img/wn/13d@2x.png'
+                }
+
+                weatherInfo.append(img)
 
             }
-            function weather(data){
-                console.log('test 2')
-                let weatherState = document.createElement('h3')
-                weatherState.textContent = data.weather_state_name
-                let currentTemp = document.createElement('h4')
-                currentTemp = data.the_temp
-                let minTemp = document.createElement('p')
-                minTemp.textContent = data.min_temp
-                let maxTemp = document.createElement('p')
-                maxTemp = data.max_temp
-
-                const weatherDiv = document.getElementById('weather-info')
-                console.log('test 3')
-                weatherDiv.append(weatherState)
-                weatherDiv.append(currentTemp)
-                weatherDiv.append(minTemp)
-                weatherDiv.append(maxTemp)
-            }
+        
+        })
 
     })
-})
 })
