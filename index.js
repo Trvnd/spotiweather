af4333fb58bba9962c1c9e10c34c1c36182de4c8
River <+>
2022-05-13 11:00:15 -0400

    more css and iframe

--- index.js
+++ index.js
@@ -17,16 +17,17 @@ document.addEventListener("DOMContentLoaded", function () {
         }, 1500);
     }, false);
 
-
-    const weatherInfo = document.getElementById('weather-list')
+    const weatherInfo = document.getElementById('weather-info')
     const img = document.getElementById('img')
     const searchBar = document.getElementById('searchbar');
     const message = document.getElementById('title')
     const playListTitle = document.getElementById('playlist')
+    
 
     searchBar.addEventListener('submit', (e) => {
         e.preventDefault()
         weatherInfo.innerHTML = ''
+        //console.log('hello')
 
         const city = searchBar.children.search.value
 
@@ -37,6 +38,7 @@ document.addEventListener("DOMContentLoaded", function () {
         })
             .then(res => res.json())
             .then(data => {
+                //console.log('test')
                 const cityData = data[0]
                 if (cityData) {
                     const woeId = cityData.woeid
@@ -55,24 +57,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
                 function weather(data) {
 
-                    const weatherState = document.createElement('li')
-                    const currentTemp = document.createElement('li')
-                    // const minTemp = document.createElement('li')
-                    // const maxTemp = document.createElement('li')
-                    const windDirection = document.createElement('li')
-                    const windSpeed = document.createElement('li')
+                    const weatherState = document.createElement('p')
+                    const currentTemp = document.createElement('p')
+                    const windDirection = document.createElement('p')
+                    const windSpeed = document.createElement('p')
 
 
                     weatherState.textContent = data.consolidated_weather[0].weather_state_name
                     currentTemp.textContent = Math.floor(data.consolidated_weather[0].the_temp)
-                    //minTemp.textContent = Math.floor(data.consolidated_weather[0].min_temp)
-                    //maxTemp.textContent = Math.floor(data.consolidated_weather[0].max_temp)
                     windDirection.textContent = data.consolidated_weather[0].wind_direction_compass
                     windSpeed.textContent = Math.floor(data.consolidated_weather[0].wind_speed)
-
-
-
-
 
 
                     weatherInfo.append(city)
@@ -85,16 +79,12 @@ document.addEventListener("DOMContentLoaded", function () {
                     weatherInfo.append(`${currentF} °F`)
                     console.log(data)
 
-
-
-
                     if (weatherState.textContent === 'Clear') {
-                        message.textContent = 'It\'s gawgeous today, get yurr butt outside!',
-                            message.src = 
+                        message.textContent = 'It\'s gawgeous today, get yurr butt outside!'
                     } if (weatherState.textContent === 'Light Cloud') {
-                        message.textContent = 'Smol cloud, might still need shades if you\'re going outside today!'
+                        message.textContent = 'Smol cloud, bring sunglasses and sunscreen outside!'
                     } if (weatherState.textContent === 'Heavy Cloud') {
-                        message.textContent = 'Lots of cloud coverage, not the best time to get a tan.'
+                        message.textContent = 'Lots of cloud coverage, low visability at times'
                     } if (weatherState.textContent === 'Showers') {
                         message.textContent = 'Isolated showers, bring an umbrella just in case!'
                     } if (weatherState.textContent === 'Light Rain') {
@@ -108,7 +98,7 @@ document.addEventListener("DOMContentLoaded", function () {
                     } if (weatherState.textContent === 'Sleet') {
                         message.textContent = 'Not quite rain, but not quite snow. Just some baby frozen ice fallilng from the sky today.'
                     } if (weatherState.textContent === 'Snow') {
-                        message.textContent = 'Time to hang with frosty, you\re getting snow today!'
+                        message.textContent = 'Time to build a frosty, you\re getting snow today!'
                     }
 
                     playListTitle.append(message)
@@ -136,10 +126,38 @@ document.addEventListener("DOMContentLoaded", function () {
                         img.src = 'http://openweathermap.org/img/wn/13d@2x.png'
 
                     }
-
-
-
                     weatherInfo.append(img)
+
+                    const spotifyPlaylist = document.getElementById('spotify')
+                    const appendPlaylist = document.createElement('iframe')
+                    //spotifyPlaylist.append(appendPlaylist)
+
+                    if (weatherState.textContent === 'Clear') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/0nXXUlBpqAW9sJngHEiNyW?utm_source=generator'
+                    } if (weatherState.textContent === 'Light Cloud') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/47QIUxFQ4H2saQM4coW9f1?utm_source=generator'
+                    } if (weatherState.textContent === 'Heavy Cloud') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/0TVjIW7fylya3umvXxW3aS?utm_source=generator'
+                    } if (weatherState.textContent === 'Showers') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/37i9dQZF1DX8eMyhtzAuvp?utm_source=generator'
+                    } if (weatherState.textContent === 'Light Rain') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/5Dq7gvImBvVo4Rv44NjEaw?utm_source=generator'
+                    } if (weatherState.textContent === 'Heavy Rain') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/4ib6u6HEQQiqmYucL2S1NH?utm_source=generator'
+                    } if (weatherState.textContent === 'Thunderstorm') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/1YcQa9V8QFIN3ylJJhDAEt?utm_source=generator'
+                    } if (weatherState.textContent === 'Hail') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/2s5QADqGWjawJqrQcsLAlF?utm_source=generator'
+                    } if (weatherState.textContent === 'Sleet') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/7fB5IlW6yBeS0SQUNIkepd?utm_source=generator'
+                    } if (weatherState.textContent === 'Snow') {
+                        iframe.src = 'https://open.spotify.com/embed/playlist/1vN04X0T4mnzp6Tgl7Irxd?utm_source=generator'
+
+                    }
+
+                    console.log(spotifyPlaylist.append(appendPlaylist))
+
+                
 
                 }
 
@@ -148,4 +166,3 @@ document.addEventListener("DOMContentLoaded", function () {
     })
 
 })
-
